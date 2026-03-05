c37cf9070c6e8f2e9e2f402cec4acc6ce6b907a4
Dpicon <darwindj2711@gmail.com>
2026-03-05 14:39:02 -0600

    1.00

---
+++ manual.py
@@ -0,0 +1,45 @@
+#PASOS 1 SI NUNCA SE INICIO GIT111111111111111111111111111111111111111111111111111
+
+    #git config --global user.name "Dpicon"
+    #git config --global user.email "darwindj2711@gmail.com"
+
+#PASOS PARA EN CARPETA LOCAL NUEVA.  222222222222222222222222222222222
+
+    #git init 
+
+#PASO 2 GREGAR CARPETA NUEVA A EL REPOSITORIO PARA QUE SE SINCRONICEN 33333333333333333333333333333333333333
+
+    #git remote add origin https://github.com/Dpicon/Mac.git
+    #git add . 
+    #git commit -m "comentario"
+    #git push
+    #git push --set-upstream origin main
+
+#PASO 3 SI HAY CAMBIO EN GIT HUB QUE NO ESTEN EN LA CARPETA SE BAJAN DE LA SIGUIENTE FORMA
+
+    #git pull origin main --allow-unrelated-histories
+
+
+
+
+
+#OTROS COMANDDOS 
+                  #git branch -M main
+                 #git push -u origin main
+               # git remote remove origin
+                #git push origin main
+
+
+# Solución rápida (si el repositorio está vacío o no te importa sobrescribir)
+# Si sabes que lo que está en GitHub no importa, puedes forzar el push:
+# git push -f origin main
+
+
+
+
+# si hay que sobreescribir git web
+
+# Opción 2 (más simple): sobrescribir GitHub
+# Si el repo en GitHub no tiene nada importante, fuerza el push:
+# git push -f origin main
+
